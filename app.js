c45bc7981751ff9a78fcf05f09ef4d20b1c667a7
Ferin Sharaf <146843308+ferinsh@users.noreply.github.com>
2024-09-16 20:22:59 +0530

    Add Validation To user sign-up

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require("bcrypt");
+const { body, validationResult } = require('express-validator');
 
 const pool = require("./db/pool");
 
@@ -65,24 +66,42 @@ app.get("/sign-up", (req, res) => {
 app.get("/", (req, res) => {
     res.render("index");
 })
-app.post("/sign-up", (req, res) => {
-    console.log(req.body);
-    // res.redirect("/");
-    bcrypt.hash(req.body.password, 10, async(err, hashedPassword) => {
-        if(err){
-            console.error("bcrypt has fail: ", err);
-            res.redirect("/");
-        }else {
-            await pool.query("INSERT INTO users (first_name, last_name, username, password) VALUES ($1, $2, $3, $4)", [
-                req.body.first_name,
-                req.body.last_name,
-                req.body.username,
-                hashedPassword
-            ])
-            res.redirect("/");
+
+app.post(
+    '/sign-up',
+    body('username').isEmail().withMessage('Invalid email format'), // Added validation for email format
+    body('password').isLength({ min: 5 }).withMessage('Password must be at least 5 characters long'), // Validating password length
+    body('confirm_password').custom((value, { req }) => { // Using correct field name for validation
+        if (value !== req.body.password) { // Compare with password
+        throw new Error('Passwords do not match'); // Throw error if passwords do not match
         }
-    })
-})
+        return true;
+    }),
+    async (req, res) => {
+        const errors = validationResult(req); // Added to handle validation errors
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+             // Send validation errors as response
+        }
+        // Handle request
+        console.log(req.body);
+        // res.redirect("/");
+        bcrypt.hash(req.body.password, 10, async(err, hashedPassword) => {
+            if(err){
+                console.error("bcrypt has fail: ", err);
+                res.redirect("/");
+            }else {
+                await pool.query("INSERT INTO users (first_name, last_name, username, password) VALUES ($1, $2, $3, $4)", [
+                    req.body.first_name,
+                    req.body.last_name,
+                    req.body.username,
+                    hashedPassword
+                ])
+                res.redirect("/");
+            }
+        })
+    },
+);
 
 
 const PORT = process.env.PORT || 3000 
